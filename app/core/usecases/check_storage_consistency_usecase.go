5f208e0f5d3b97a902a74d69f88c0ff06bcaf6b1
mkafonso <mkafonso.dev@gmail.com>
2026-04-01 22:27:29 -0300

    feat: add storage consistency check use case

---
+++ app/core/usecases/check_storage_consistency_usecase.go
@@ -0,0 +1,45 @@
+package usecases
+
+import (
+	"context"
+
+	"github.com/mkafonso/goledger-challenge-besu/core/errors"
+	"github.com/mkafonso/goledger-challenge-besu/core/ports"
+)
+
+type CheckStorageConsistencyRequest struct{}
+
+type CheckStorageConsistencyResponse struct {
+	Consistent bool
+}
+
+type CheckStorageConsistency struct {
+	blockchain ports.StorageBlockchainProvider
+	repository ports.StorageRepository
+}
+
+func NewCheckStorageConsistency(
+	blockchain ports.StorageBlockchainProvider,
+	repository ports.StorageRepository,
+) *CheckStorageConsistency {
+	return &CheckStorageConsistency{
+		blockchain: blockchain,
+		repository: repository,
+	}
+}
+
+func (uc *CheckStorageConsistency) Execute(ctx context.Context, _ *CheckStorageConsistencyRequest) (*CheckStorageConsistencyResponse, error) {
+	blockchainValue, err := uc.blockchain.GetStorageFromBlockchain(ctx)
+	if err != nil {
+		return nil, errors.NewErrorUnableToReadFromBlockchain()
+	}
+
+	dbValue, err := uc.repository.GetStorage(ctx)
+	if err != nil {
+		return nil, errors.NewInternalError()
+	}
+
+	return &CheckStorageConsistencyResponse{
+		Consistent: blockchainValue == dbValue,
+	}, nil
+}
---
+++ app/core/usecases/check_storage_consistency_usecase_test.go
@@ -0,0 +1,66 @@
+package usecases_test
+
+import (
+	"context"
+	"testing"
+
+	memory_providers "github.com/mkafonso/goledger-challenge-besu/__tests__/providers"
+	memory_repositories "github.com/mkafonso/goledger-challenge-besu/__tests__/repositories"
+	usecase "github.com/mkafonso/goledger-challenge-besu/core/usecases"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCheckStorageConsistency_ShouldBeConsistent(t *testing.T) {
+	blockchain := memory_providers.NewMemoryStorageBlockchainProvider(100)
+	repository := memory_repositories.NewMemoryStorageRepositoryProvider(100)
+
+	uc := usecase.NewCheckStorageConsistency(blockchain, repository)
+
+	response, err := uc.Execute(context.Background(), &usecase.CheckStorageConsistencyRequest{})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.True(t, response.Consistent)
+}
+
+func TestCheckStorageConsistency_ShouldBeInconsistent(t *testing.T) {
+	blockchain := memory_providers.NewMemoryStorageBlockchainProvider(100)
+	repository := memory_repositories.NewMemoryStorageRepositoryProvider(50)
+
+	uc := usecase.NewCheckStorageConsistency(blockchain, repository)
+
+	response, err := uc.Execute(context.Background(), &usecase.CheckStorageConsistencyRequest{})
+
+	assert.NoError(t, err)
+	assert.NotNil(t, response)
+	assert.False(t, response.Consistent)
+}
+
+func TestCheckStorageConsistency_ShouldMapBlockchainError(t *testing.T) {
+	blockchain := memory_providers.NewMemoryStorageBlockchainProvider(0)
+	repository := memory_repositories.NewMemoryStorageRepositoryProvider(0)
+	blockchain.ForceError = true
+
+	uc := usecase.NewCheckStorageConsistency(blockchain, repository)
+
+	response, err := uc.Execute(context.Background(), &usecase.CheckStorageConsistencyRequest{})
+
+	assert.Nil(t, response)
+	assert.Error(t, err)
+	assert.Equal(t, "unable to read storage from blockchain", err.Error())
+}
+
+func TestCheckStorageConsistency_ShouldMapRepositoryError(t *testing.T) {
+	blockchain := memory_providers.NewMemoryStorageBlockchainProvider(100)
+	repository := memory_repositories.NewMemoryStorageRepositoryProvider(0)
+	repository.ForceError = true
+
+	uc := usecase.NewCheckStorageConsistency(blockchain, repository)
+
+	response, err := uc.Execute(context.Background(), &usecase.CheckStorageConsistencyRequest{})
+
+	assert.Nil(t, response)
+	assert.Error(t, err)
+	assert.Equal(t, "internal server error", err.Error())
+}
